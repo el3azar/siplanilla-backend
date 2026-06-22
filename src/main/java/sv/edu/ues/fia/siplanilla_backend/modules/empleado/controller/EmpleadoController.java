2d25e1f3b9fa94ae80b662a7e04aa6063c3976e9
el3azar <otoeleazarivas@gmail.com>
2026-06-21 21:08:47 -0600

    resolviendo problemas de roles

--- src/main/java/sv/edu/ues/fia/siplanilla_backend/modules/empleado/controller/EmpleadoController.java
+++ src/main/java/sv/edu/ues/fia/siplanilla_backend/modules/empleado/controller/EmpleadoController.java
@@ -10,7 +10,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -18,7 +17,6 @@ import java.util.List;
 @RequestMapping("/v1/empleados")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
-@PreAuthorize("hasAnyRole('Administrador','RRHH')")
 public class EmpleadoController {
 
     private final EmpleadoService empleadoService;
--- src/main/java/sv/edu/ues/fia/siplanilla_backend/modules/seguridad/controller/RolController.java
+++ src/main/java/sv/edu/ues/fia/siplanilla_backend/modules/seguridad/controller/RolController.java
@@ -3,7 +3,6 @@ package sv.edu.ues.fia.siplanilla_backend.modules.seguridad.controller;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/roles")
 @RequiredArgsConstructor
-@PreAuthorize("hasRole('Administrador')")
 @Tag(name = "Administración - Roles")
 public class RolController {
 
